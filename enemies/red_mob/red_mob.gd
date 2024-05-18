0bc97d01eb2c2950b240827568b80a69e1b9fa74
yumurta2 <baturoski@gmail.com>
2024-05-18 17:09:54 +0300

    mob move

--- enemies/red_mob/red_mob.gd
+++ enemies/red_mob/red_mob.gd
@@ -1,6 +1,9 @@
-extends Node2D
-const speed = 200
-
+extends CharacterBody2D
+const speed = 30
+var time_accumulator = 0.0
+var update_interval = 0.5
+var directionX = 0
+var directionY = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +13,23 @@ func _ready():
 func _process(delta):
 	pass
 func _physics_process(delta):
-	var directionX = randi_range(-1,1)
-	var directionY = randi_range(-1,1)
-	position.x += directionX * speed * delta
-	position.y += directionY * speed * delta
+	if directionX:
+		velocity.x = directionX * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	if directionY:
+		velocity.y = directionY * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+	move_and_slide()
+
+	time_accumulator += delta
+	if time_accumulator >= update_interval:
+		time_accumulator -= update_interval
+		perform_update(delta)
+		
+func perform_update(delta):
+	directionX = randi_range(-1,1) 
+	directionY = randi_range(-1,1)
+func damage(num):
+	pass
