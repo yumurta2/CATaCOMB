532763c61dfdbc7c8cf7651c38c3bce11f7b818b
yumurta2 <baturoski@gmail.com>
2024-05-18 13:39:21 +0300

    asd

--- player/player.gd
+++ player/player.gd
@@ -2,38 +2,58 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-@export var SPEED = 100.0
+@export var WALK_SPEED = 100.0
+@export var ROLL_SPEED = 300.0
+const ROLL_TIME = 0.3
 var last_direction
+var dodging = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-
-
+var directionX 
+var directionY
 func _physics_process(delta):
-	# Add the gravity.
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionX = Input.get_axis("move_left", "move_right")
-	var directionY = Input.get_axis("move_up", "move_down")
-	
-	if directionX == -1:
-		last_direction = "left"
-	elif directionX == 1:
-		last_direction = "right"
-	elif directionY == -1:
-		last_direction = "up"
-	elif directionY == 1:
-		last_direction = "down"
-	change_animation(last_direction)
-		
-	if directionX:
-		velocity.x = directionX * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if not dodging:
+		directionX = Input.get_axis("move_left", "move_right")
+		directionY = Input.get_axis("move_up", "move_down")
+
+		if directionX == -1:
+			last_direction = "left"
+		elif directionX == 1:
+			last_direction = "right"
+		elif directionY == -1:
+			last_direction = "up"
+		elif directionY == 1:
+			last_direction = "down"
+		change_animation(last_direction)
+
+		if directionX:
+			velocity.x = directionX * WALK_SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
+		if directionY:
+			velocity.y = directionY * WALK_SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, WALK_SPEED)
+
+		if Input.is_action_just_pressed("roll"):
+			dodge(last_direction)
+			dodging = true
+			await get_tree().create_timer(ROLL_TIME).timeout  # adjust the time as needed
+			dodging = false
 	move_and_slide()
+func dodge(last_direction):
+	if last_direction == "left":
+		animated_sprite.play("roll_right")
+		animated_sprite.flip_h = true
+		velocity.x = -1 * ROLL_SPEED
+	elif last_direction == "right":
+		animated_sprite.play("roll_right")
+		animated_sprite.flip_h = false
+		velocity.x = 1 * ROLL_SPEED
+	elif last_direction == "up":
+		velocity.y = -1 * ROLL_SPEED
+	elif last_direction == "down":
+		velocity.y = 1 * ROLL_SPEED
 	
 func change_animation(last_direction):
 	if last_direction == "left":
